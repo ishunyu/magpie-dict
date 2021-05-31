263fe4cdc31a43c3dae7a251f15cdf7bb847478a
Shun Yu <shunsmailbox@gmail.com>
2021-05-30 20:51:13 -0700

    Exuecte Compare Script

--- pkg/server/rest.go
+++ pkg/server/rest.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
 	"time"
 )
@@ -27,6 +32,11 @@ type searchResponseData struct {
 	Subs    []*Record `json:"subs"`
 }
 
+type compareResponse struct {
+	Status string `json:"status"`
+	Output string `json:"output"`
+}
+
 func ShowsHandler(index *Index) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		handleShows(w, req, index)
@@ -42,6 +52,12 @@ func GetSearchHandler(index *Index) func(http.ResponseWriter, *http.Request) {
 func SubsHandler(index *Index) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		handleSubs(w, req, index)
+	}
+}
+
+func CompareHandler(tmpPath string, comparePath string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		handleCompare(w, req, tmpPath, comparePath)
 	}
 }
 
@@ -120,6 +136,58 @@ func handleSubs(w http.ResponseWriter, req *http.Request, index *Index) {
 
 	data, _ := json.Marshal(response)
 	fmt.Fprintf(w, string(data))
+}
+
+func handleCompare(w http.ResponseWriter, req *http.Request, tmpDir string, comparePath string) {
+	compareDir := filepath.Join(tmpDir, "compare")
+	os.MkdirAll(compareDir, 0700)
+	ms := time.Now().UnixNano() / int64(time.Millisecond)
+	dir, _ := ioutil.TempDir(compareDir, strconv.FormatInt(ms, 10)+"_")
+	fmt.Println(dir)
+
+	chinese_file := getAndSaveFile(req, "CHINESE_FILE", dir, "chinese_file.sbv")
+	original_file := getAndSaveFile(req, "ORIGINAL_FILE", dir, "original_file.sbv")
+	revised_file := getAndSaveFile(req, "REVISED_FILE", dir, "revised_file.sbv")
+
+	fmt.Println(chinese_file)
+	fmt.Println(original_file)
+	fmt.Println(revised_file)
+
+	output_file := filepath.Join(dir, "output.xlsx")
+	cmd := exec.Command("python", comparePath, "-o", output_file, original_file, revised_file, chinese_file)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(output_file)
+
+	bytes, err := ioutil.ReadFile(output_file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	w.Write(bytes)
+}
+
+func getAndSaveFile(req *http.Request, key string, dir string, filename string) string {
+	formFile, _, err := req.FormFile(key)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	bytes, _ := ioutil.ReadAll(formFile)
+	file := filepath.Join(dir, filename)
+	err = ioutil.WriteFile(file, bytes, 0700)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return file
 }
 
 func retreiveResponse(data *Data, result *recordID) *searchResponseData {
